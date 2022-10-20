6ee2b5811102a9807a0387f5030e1c623ff1d8ef
Nandhini <nandhinimurugesan03@gmail.com>
2022-10-20 14:10:29 +0530

    After changing admin ui,Admin portal Testing Done

--- AdminTesting/src/test/java/com/test/NewTest.java
+++ AdminTesting/src/test/java/com/test/NewTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class NewTest {
-	public String baseUrl = "http://localhost:4201";
+	public String baseUrl = "http://localhost:4201/";
 
 	String driverPath = "C:\\MyDownloads\\geckodriver-v0.31.0-win64\\geckodriver.exe";
 	public WebDriver driver;
@@ -32,7 +32,7 @@ public class NewTest {
 		driver.findElement(By.xpath("//*[@id=\"inputPassword\"]")).sendKeys("123456");
 		// Login Button
 		driver.findElement(By.xpath("/html/body/app-root/app-login/div/form/button")).click();
-		String actualUrl = "http://localhost:4200/user-account";
+		String actualUrl = "http://localhost:4201/smaple";
 		String expectedUrl = driver.getCurrentUrl();
 		if (actualUrl.equalsIgnoreCase(expectedUrl)) {
 			System.out.println("Login Successful");
@@ -41,76 +41,97 @@ public class NewTest {
 	}
 
 	@Test(priority = 1)
-	public void checkbookRequests() {
+	public void userAccountInfo() throws InterruptedException {
 		// Checkbook Request Hyperlink
-		driver.findElement(By.xpath("/html/body/app-root/div/nav/div/ul/li[2]/a")).click();
-		// Confirm Request Button
-		driver.findElement(By.xpath("/html/body/app-root/app-checkbook-requests/table/tbody/tr/td[6]/button")).click();
-		System.out.println("ChequeRequest Confirmed");
+		driver.findElement(
+				By.xpath("	/html/body/app-root/app-sample/main/div/div/div[2]/div/div/div[2]/div[1]/a/span/span[2]"))
+				.click();
+		Thread.sleep(300);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println("userAccount Displayed");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		// home
+		driver.findElement(By.xpath("//*[@id=\"ftco-nav\"]/ul/li/a")).click();
 
 	}
 
-	@Test(priority = 5)
-	public void loanRequests() throws InterruptedException {
+	@Test(priority = 2)
+	public void checkbookRequests() throws InterruptedException {
 		// Checkbook Request Hyperlink
-		driver.findElement(By.xpath("/html/body/app-root/div/nav/div/ul/li[3]/a")).click();
-		// Confirm Request Button
-		driver.findElement(By.xpath("//*[@id=\"update\"]")).click();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector("button.btn:nth-child(2)")).click();
-
-		// switch focus to alert
-		Alert a = driver.switchTo().alert();
-		// get alert text
-		String s = driver.switchTo().alert().getText();
-		System.out.println("Alert text is: " + s);
-
+		driver.findElement(
+				By.xpath("/html/body/app-root/app-sample/main/div/div/div[2]/div/div/div[2]/div[2]/a/span/span[2]"))
+				.click();
 		Thread.sleep(300);
-		// accepting alert
-		a.accept();
-		System.out.println("loanRequest Confirmed");
+		// Confirm Request Button
+		driver.findElement(By.xpath("/html/body/app-root/app-checkbook-requests/table/tbody/tr/td[6]/button")).click();
+		System.out.println("ChequeRequest Confirmed");
+		// home
+		driver.findElement(By.xpath("//*[@id=\"ftco-nav\"]/ul/li/a")).click();
 
+	}
+
+	@Test(priority = 3)
+	public void transactionHistory() throws InterruptedException {
+
+		// Transaction history nav link
+		driver.findElement(By.xpath("/html/body/app-root/app-sample/main/div/div/div[2]/div/div/div[2]/div[5]/a/span"))
+				.click();
+		Thread.sleep(300);
+		System.out.println("transactionHistory displayed");
+
+		driver.findElement(By.xpath("//*[@id=\"ftco-nav\"]/ul/li/a")).click();
+	}
+
+	@Test(priority = 4)
+	public void transferHistory() throws InterruptedException {
+
+		// Transfer history nav link
+		driver.findElement(
+				By.xpath("/html/body/app-root/app-sample/main/div/div/div[2]/div/div/div[2]/div[6]/a/span/span[2]"))
+				.click();
+		Thread.sleep(300);
+		System.out.println("transferHistory displayed");
+
+		driver.findElement(By.xpath("//*[@id=\"ftco-nav\"]/ul/li/a")).click();
 	}
 
 	@Test(priority = 4)
 	public void authorization() throws InterruptedException {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-		// Authorization link
-		driver.findElement(By.xpath("/html/body/app-root/div/nav/div/ul/li[4]/a")).click();
+		// Authorization nav link
+		driver.findElement(By.xpath("/html/body/app-root/app-sample/main/div/div/div[2]/div/div/div[2]/div[4]/a/span"))
+				.click();
 		// Create Account Button
 		driver.findElement(By.xpath("/html/body/app-root/app-authorize-registration/table/tbody/tr/td[9]")).click();
 		System.out.println("Authorized");
-//	Cancel Button
+//		// Cancel Button
 //		driver.findElement(By.xpath("/html/body/app-root/app-authorize-registration/table/tbody/tr[2]/td[10]/button"))
 //				.click();
 //		System.out.println(" Not Authorized");
+		System.out.println("AuthorizeKyc Works");
+		driver.findElement(By.xpath("//*[@id=\"ftco-nav\"]/ul/li/a")).click();
 
 	}
 
-	@Test(priority = 2)
-	public void transactionHistory() throws InterruptedException {
-
-		// Transaction history nav link
-		driver.findElement(By.xpath("/html/body/app-root/div/nav/div/ul/li[5]/a")).click();
-		System.out.println("transactionHistory displayed");
+	@Test(priority = 5)
+	public void loanRequests() throws InterruptedException {
+		// Loan Request Hyperlink
+		driver.findElement(By.xpath("/html/body/app-root/app-sample/main/div/div/div[2]/div/div/div[2]/div[3]/a/span"))
+				.click();
 		Thread.sleep(300);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println("loanRequest Displayed");
+		driver.findElement(By.xpath("//*[@id=\"ftco-nav\"]/ul/li/a")).click();
+
 	}
 
-	@Test(priority = 3)
-	public void transferHistory() throws InterruptedException {
-
-		// Transfer history nav link
-		driver.findElement(By.xpath("/html/body/app-root/div/nav/div/ul/li[6]/a")).click();
-		System.out.println("transferHistory displayed");
-		Thread.sleep(300);
+	@Test(priority = 6)
+	public void logout() {
+		// LogOut Button
+		driver.findElement(
+				By.xpath("/html/body/app-root/app-sample/main/div/div/div[2]/div/div/div[2]/div[7]/a/span/span[1]"))
+				.click();
+		System.out.println("Logged Out");
 	}
-
-//	@Test(priority = 6)
-//	public void logout() {
-//		// LogOut Button
-//		driver.findElement(By.xpath("/html/body/app-root/div/nav/div/ul/div/li/a")).click();
-//		System.out.println("Logged Out");
-//	}
 }
-
